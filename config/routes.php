20847899c2ed66624f63a0bf41d74d14a8aaed5d
Mateusz Tymek <mtymek@gmail.com>
2014-08-06 16:16:00 +0200

    Default parameter

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,9 @@ return [
         'name' => 'hello [<name>]',
         'description' => 'Show welcome message',
         'short_description' => 'Show welcome message',
+        'defaults' => [
+            'name' => 'World',
+        ],
         'handler' => 'Application\Command\Hello',
     ]
 ];
